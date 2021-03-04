5cfca6fda5989a20c6cd8ff89ab9db6da8414a77
Anderson Joel <amacarlupup@gmail.com>
2021-03-04 11:19:35 -0500

    Primer commit

--- config/db.js
+++ config/db.js
@@ -3,20 +3,20 @@ const db = {};
 
 
 // Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('DB_A671D0_richardnox', 'DB_A671D0_richardnox_admin', 'ASPDEMO2020',
-  {
-    host: 'sql5083.site4now.net',
-    dialect: 'mssql',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+// const sequelize = new Sequelize('DB_A671D0_richardnox', 'DB_A671D0_richardnox_admin', 'ASPDEMO2020',
+//   {
+//     host: 'sql5083.site4now.net',
+//     dialect: 'mssql',
+//     logging: false,
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
 
-  }
-);
+//   }
+// );
 
 // Verificar coneccion a la base de datos
 async function autenticacion() {
