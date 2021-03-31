51b1926922815d8bb1e12548eba9af56a5ee7df3
Anderson <54516856+amacarlupu@users.noreply.github.com>
2021-03-31 17:52:44 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,20 +3,7 @@ const db = {};
 
 
 // Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('DB_A671D0_richardnox', 'DB_A671D0_richardnox_admin', 'ASPDEMO2020',
-  {
-    host: 'sql5083.site4now.net',
-    dialect: 'mssql',
-    logging: false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-
-  }
-);
+// Oculto por seguridad
 
 // Verificar coneccion a la base de datos
 async function autenticacion() {
